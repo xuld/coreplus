bfe5b77663a93433f8c209eb047f19e61306d0cf
unknown <Administrator@20100705-1146.(none)>
2011-12-15 20:37:50 +0800

    测试

--- Samples/CoreSample/Program.cpp
+++ Samples/CoreSample/Program.cpp
@@ -11,6 +11,11 @@ int main(int argc, _TCHAR* argv[])  {
 
 	cout<<CString::GetLength(TEXT("A"));
 
+	
+	// zhe li shi ceshi   
+
+	// CString jiu shi yao wan cheng de diyigelei
+
 
 	system("pause");
 	return 0;
